e61ddb9bdd6d15ad2ea70c6dcce9b6d6daaf9909
Daniel Jarrett <danieljarrett74@gmail.com>
2022-03-20 17:56:44 +1100

    packaging using setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='notification-api-client',
+    version='0.1',
+    packages=['client'],
+    license='MIT',
+    description='Notification API Client',
+    long_description=open('README.md').read(),
+    install_requires=['httpx>=0.15.4,<0.23.0','attrs>=21.3.0','python-dateutil==^2.8.0"'],
+    url='https://github.com/danieljarrett74/notification-api-python-client',
+    author='Daniel Jarrett',
+    author_email='danieljarrett74@gmail.com',
+) 
