468582d7f1ced93031df8019d155c2a45aeca54e
Daniel Jarrett <danieljarrett74@gmail.com>
2022-03-20 18:13:23 +1100

    removed double quote

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='Notification API Client',
     long_description=open('README.md').read(),
-    install_requires=['httpx>=0.15.4,<0.23.0','attrs>=21.3.0','python-dateutil==2.8.2"'],
+    install_requires=['httpx>=0.15.4,<0.23.0','attrs>=21.3.0','python-dateutil==2.8.2'],
     url='https://github.com/danieljarrett74/notification-api-python-client',
     author='Daniel Jarrett',
     author_email='danieljarrett74@gmail.com',
